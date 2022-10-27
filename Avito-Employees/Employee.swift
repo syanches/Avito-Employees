a5790a43e1dd9d9e057d88d9e9896bffbb8c70d1
Artem Martirosyan <syanches@gmail.com>
2022-10-28 00:22:36 +0300

    added first working version of fetching from remote resource mocks are deleted

--- Avito-Employees/Employee.swift
+++ Avito-Employees/Employee.swift
@@ -5,21 +5,23 @@
 //  Created by Artem Martirosyan on 26.10.2022.
 //
 
-struct Employee {
+struct Employee: Decodable {
     let name: String
     let phoneNumber: String
     let skills: [String]
     
-    static func EmployeesMock() -> [Employee] {
-        return [
-            Employee(name: "John", phoneNumber: "769453", skills: ["Swift", "iOS"]),
-            Employee(name: "Diego", phoneNumber: "987924", skills: ["Kotlin", "Android"]),
-            Employee(name: "Alfred", phoneNumber: "452533", skills: ["Objective-C", "Android", "Photoshop", "Objective-C", "Android"]),
-            Employee(name: "John", phoneNumber: "212456", skills: ["Java", "Python"]),
-            Employee(name: "Mat", phoneNumber: "778975", skills: ["Android", "MovieMaker"]),
-            Employee(name: "Bob", phoneNumber: "456468", skills: ["Groovy", "Kotlin"]),
-            Employee(name: "Marty", phoneNumber: "321789", skills: ["Android", "PHP", "C#"]),
-        ]
-            .sorted(by: {$0.name < $1.name})
+    enum CodingKeys: String, CodingKey {
+        case name
+        case phoneNumber = "phone_number"
+        case skills
     }
+}
+
+struct Company: Decodable {
+    let name: String
+    let employees: [Employee]
+}
+
+struct CompanyItem: Decodable {
+    let company: Company
 }
--- Avito-Employees/EmployeesViewController.swift
+++ Avito-Employees/EmployeesViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class EmployeesViewController: UIViewController {
     
-    let employees = Employee.EmployeesMock()
+    var employeesParsed: [Employee] = []
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -28,7 +28,9 @@ class EmployeesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
+        fetchEmployees()
     }
+    
 }
 
 // MARK: - Private Methods
@@ -37,13 +39,41 @@ extension EmployeesViewController {
         title = "Employees"
         view.backgroundColor = .systemBackground
         view.addSubview(tableView)
-                
+        
         NSLayoutConstraint.activate([
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    private func fetchEmployees() {
+        guard let url = URL(string:"https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c") else { return }
+        let dataTask = URLSession.shared.dataTask(with: url) {[weak self] (data, response, error) in
+            if let data = data, (response as? HTTPURLResponse)?.statusCode == 200 {
+                self?.parseData(from: data)
+            }
+            if let error = error {
+                print(error)
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private func parseData(from data: Data) {
+        do {
+            let jsonObject = try JSONDecoder().decode(CompanyItem.self, from: data)
+            for employee in jsonObject.company.employees {
+                employeesParsed.append(Employee(name: employee.name, phoneNumber: employee.phoneNumber, skills: employee.skills))
+            }
+            employeesParsed.sort(by: {$0.name < $1.name})
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+        } catch {
+            print("JSON parsing error: ", error)
+        }
     }
 }
 
@@ -54,12 +84,12 @@ extension EmployeesViewController: UITableViewDelegate {
 //MARK: - TableView DataSource
 extension EmployeesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        employees.count
+        employeesParsed.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as? EmployeesTableViewCell else { return UITableViewCell() }
-        cell.initCell(name: employees[indexPath.row].name, phoneNumber: employees[indexPath.row].phoneNumber, skills: employees[indexPath.row].skills)
+        cell.initCell(name: employeesParsed[indexPath.row].name, phoneNumber: employeesParsed[indexPath.row].phoneNumber, skills: employeesParsed[indexPath.row].skills)
         return cell
     }
 }
